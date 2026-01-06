2ad175772a98ff70459bfd1b619ed581e2286d32
aakash2398 <a7gautam@gmail.com>
2026-01-06 14:45:23 +0530

    image updated

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { Github, Linkedin, Download, Mail } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import profileImage from "@/assets/profile1.png";
+import profileImage from "@/assets/Profile.png";
 import resumefile from"@/assets/Aakash Kumar Resume.pdf";
 
 export function HeroSection() {
